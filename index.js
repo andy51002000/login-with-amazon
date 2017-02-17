601be23efd19f952687a19489f16d7d242b19715
Steve Gula <stevegula@caremonit.com>
2017-02-17 17:06:14 -0600

    broke out callback + promise

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 var https = require("https");
  /**
   * @param string $accessToken Required
-  * @param string $key Optional Lets you namespace the return value
   * @return object {data} or {key: data} depending on options provided
   */
-exports.getProfilePromise = function (accessToken, key) {
+exports.getProfilePromise = function (accessToken) {
   return new Promise(function (resolve, reject) {
-    var url = "https://api.amazon.com/user/profile?access_token="+
-                  encodeURIComponent(accessToken);
-    var req = https.get(url, function (res) {
-      var body = "";
-      res.setEncoding("utf8");
-      res.on("data", function (chunk) {
-        body += chunk;
-      });
-      res.on("end", function () {
-        var data = JSON.parse(body);
-        if(data.error_description) {
-          console.error("Error getting Amazon Profile - "+JSON.stringify(body));
-          return reject(new Error(body));
-        } else {
-          var result = {};
-          if (key) {
-            result[key] = data;
-          } else {
-            result = data;
-          }
-          resolve(result);
-        }
-      });
-    }).on("error", function (err) {
-      console.error("Error getting Amazon Profile - "+JSON.stringify(err));
-      return reject(err);
+    exports.getProfileCallback(accessToken, function (err, data){
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(data);
+      }
     });
-    req.end();
   });
+};
+
+exports.getProfileCallback = function (accessToken, callback) {
+  var url = "https://api.amazon.com/user/profile?access_token="+
+                encodeURIComponent(accessToken);
+  var req = https.get(url, function (res) {
+    var body = "";
+    res.setEncoding("utf8");
+    res.on("data", function (chunk) {
+      body += chunk;
+    });
+    res.on("end", function () {
+      var data = JSON.parse(body);
+      callback(null, data);
+    });
+  }).on("error", function (err) {
+    console.error("Error getting Amazon Profile - "+JSON.stringify(err));
+    callback(err);
+  });
+  req.end();
 };
